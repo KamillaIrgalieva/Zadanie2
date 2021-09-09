1045ae75f921e992222d1b76b330320c49d8154c
IrgalievaKM <kama270290@yandex.ru>
2021-09-09 16:47:53 +0300

    Initial commit

---
+++ src/com/company/OnTaskDoneListener.java
@@ -0,0 +1,6 @@
+package com.company;
+
+@FunctionalInterface  //аннотация - проверяет, чтобы было не более одного абстрактного метода
+public interface OnTaskDoneListener {  //функциональный интерфейс, с одним абстрактным методом
+    void onDone(String result);  //абстрактный метод без реализации
+}
